726b709d3ea542c36096580874b16c97900a5d09
Dragonblaze123 <96932175+Dragonblaze123@users.noreply.github.com>
2022-01-14 14:37:45 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,10 +4,4 @@ sudo rename git github
 sudo rename cp copy
 sudo rename ls list
 sudo rename rm del
-cd /usr/share/rpd-wallpaper
-sudo rename raspberry-pi-logo.png rasip-ori.png
-cd
-cd Lime-OS
-cd files
-sudo copy raspberry-pi-logo.png /usr/share/rpd-wallpaper
-sudo reboot
+
