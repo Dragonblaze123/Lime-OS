b3a27a54e5c3ad8afc8538def82af1b2cceb87c6
Dragonblaze123 <96932175+Dragonblaze123@users.noreply.github.com>
2022-01-14 12:05:06 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,9 +5,9 @@ sudo rename cp copy
 sudo rename ls list
 sudo rename rm del
 cd /usr/share/rpd-wallpaper
-sudo mv raspberry-pi-logo.png rasip-ori.png
+sudo rename raspberry-pi-logo.png rasip-ori.png
 cd
 cd Lime-OS
 cd files
-sudo cp raspberry-pi-logo.png /usr/share/rpd-wallpaper
+sudo copy raspberry-pi-logo.png /usr/share/rpd-wallpaper
 sudo reboot
