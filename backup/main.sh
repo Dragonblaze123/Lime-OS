333c670ffc2140e4c38b1124fc5d34f1d12e1d2c
Dragonblaze123 <96932175+Dragonblaze123@users.noreply.github.com>
2022-01-15 11:41:45 +0530

    Update main.sh

--- backup/main.sh
+++ backup/main.sh
@@ -1,1 +1,9 @@
-
+cd /bin
+sudo mv mv rename
+sudo rename git github
+sudo rename cp copy
+sudo rename ls list
+sudo rename rm del
+sudo rename nano edit
+sudo rename mkdir nfold
+sudo rename cat view
